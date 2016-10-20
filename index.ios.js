5acbc3f51211f7f8acd4f77e9f445d659d26c1fb
DEBICI Sébastien <sebastien.debici@gmail.com>
2016-10-20 19:15:12 +0200

    last commit

--- index.ios.js
+++ index.ios.js
@@ -9,11 +9,18 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
+  TouchableHighlight
 } from 'react-native';
+
 
 export default class Jobber extends Component {
   render() {
+    const routes = [
+      {title: 'First Scene', index: 0},
+      {title: 'Second Scene', index: 1},
+    ];
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -24,8 +31,24 @@ export default class Jobber extends Component {
         </Button>
         <Button style={styles.buttonInscription}>
           Registration
-        </Button>
-        
+        </Button>  
+
+        <Navigator
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
+          renderScene={(route, navigator) =>
+            <TouchableHighlight onPress={() => {
+              if (route.index === 0) {
+                navigator.push(routes[1]);
+              } else {
+                navigator.pop();
+              }
+            }}>
+            <Text>Hello {route.title}!</Text>
+            </TouchableHighlight>
+          }
+          style={{padding: 100}}
+        />
         
       </View>
     );
